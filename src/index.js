26a9af0a0ae6cdb6abb784d34890579dfd2a5580
renevds <rene.v.d.s.9@gmail.com>
2022-07-30 16:40:18 +0200

    Export

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
-import React from 'react';
-import ReactDOM from "react-dom/client";
-
-import './index.css';
 import SaleForPeriodChart from "./components/ProjectPage/SaleForPeriodChart";
 import VolumeChart from "./components/ProjectPage/VolumeChart";
 import FloorPriceChart from "./components/ProjectPage/FloorPriceChart";
@@ -12,36 +8,15 @@ import HoldersChart from "./components/ProjectPage/HoldersChart";
 import VolatilitySpeedometer from "./components/Partials/Charts/instances/VolatilitySpeedometer";
 import CompactFloorPriceChart from "./components/Partials/Charts/instances/CompactFloorPriceChart";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const addr = "0x23581767a106ae21c074b2276D25e5C3e136a68b"
-
-root.render(
-  <div className="projectpage__row">
-    <VolatilitySpeedometer contractAddress={addr}/>
-    <div style={{width: '200px', height: '100px'}}>
-      <CompactFloorPriceChart contractAddress={addr}/>
-    </div>
-    <div className="projectpage__col">
-      <SaleForPeriodChart address={addr}/>
-    </div>
-    <div className="projectpage__col">
-      <VolumeChart address={addr}/>
-    </div>
-    <div className="projectpage__col">
-      <FloorPriceChart address={addr}/>
-    </div>
-    <div className="projectpage__col">
-      <ProfitPerSaleChart address={addr}/>
-    </div>
-    <div className="projectpage__col__half">
-      <HoldingAmountDistributionChart address={addr}/>
-    </div>
-    <div className="projectpage__col__half">
-      <HoldingPeriodDistributionChart address={addr}/>
-    </div>
-    <div className="projectpage__col">
-      <HoldersChart address={addr}/>
-    </div>
-  </div>
-);
+export {
+  SaleForPeriodChart,
+  VolumeChart,
+  FloorPriceChart,
+  ProfitPerSaleChart,
+  HoldingAmountDistributionChart,
+  HoldingPeriodDistributionChart,
+  HoldersChart,
+  VolatilitySpeedometer,
+  CompactFloorPriceChart
+};
